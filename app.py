3c39b940edd1e73097494bfb021e55863e95f6d8
sasha-fedorov <36fedorov@gmail.com>
2025-10-31 11:35:11 +0100

    Add console app template

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/command', methods=['POST'])
+def command():
+    data = request.get_json()
+    cmd = data.get('command', '').strip().lower()
+
+    if cmd == 'hello':
+        response = "Hello world!"
+    else:
+        response = f"Unknown command: {cmd}"
+    
+    return jsonify({'response': response})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
