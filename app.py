cd2207c4604547c7eed4f312b63f5126a2f349f6
sasha-fedorov <36fedorov@gmail.com>
2025-10-31 13:00:49 +0100

    Add pymongo

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
+import os
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
 
 load_dotenv()
 
 app = Flask(__name__)
 
+CONNECTION_STRING = os.getenv('MONGODB_CONNECTION_STRING')
+client = MongoClient(CONNECTION_STRING)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/command', methods=['POST'])
 def command():
@@ -16,10 +23,16 @@ def command():
 
     if cmd == 'hello':
         response = "Hello world!"
+    elif cmd == 'ping':
+        if client.db_name.command('ping'):
+            response = "Database connected!"
+        else:
+            response = "Database not connected :("
     else:
         response = f"Unknown command: {cmd}"
-    
+
     return jsonify({'response': response})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
